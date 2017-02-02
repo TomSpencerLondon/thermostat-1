963e99945d54b616bcf9c0fa55d2394f1bdd5973
PeteGH <peter@grant-hay.eu>
2017-02-02 18:39:54 +0100

    adding weather API

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
 
     var thermostat = new Thermostat();
+
+    $('#select-city').submit(function(event) {
+      event.preventDefault();
+      var city = $('#current-city').val();
+        displayWeather(city);
+    });
+
     updateTemperature();
   $('#up').on('click', function() {
     thermostat.increase();
@@ -24,10 +31,18 @@ $('.onoffswitch-label').click(function() {
       thermostat.powerSaveOff();
   }
   $('#powersaving').text(thermostat.isPowerSave());
-})
 
+});
   function updateTemperature() {
     $('#currenttemp').text(thermostat._temp);
     $('#currenttemp').attr('class', thermostat.energyUsage());
+  }
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&APPID=07549dd4f855daf2a59beb7db97b47a0';
+    var units = '&units=metric';
+    $.get( url + token + units, function(data) {
+      $('#Ambient-temperature').text(data.main.temp);
+    });
   }
 });
