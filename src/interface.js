819b7d948e4547d44a0c9d7cd2926e29238ee3f3
PeteGH <peter@grant-hay.eu>
2017-02-02 16:29:08 +0100

    adding colour to power usage with working powersave toggle

--- src/interface.js
+++ src/interface.js
@@ -17,40 +17,17 @@ $(document).ready(function() {
     updateTemperature();
   });
 
-  // $('.onoffswitch-checkbox').change(function() {
-  //   $('#powersaving').text(jump);
-  // }
-//   if ( $('.onoffswitch-checkbox').is(':checked') ) {
-//     $('#powersaving').html('jump');
-//   }
-//   else if ( $('.onoffswitch-checkbox').not(':checked') ) {
-//     $('#powersaving').html('off');
-// }
-
-//   if ( $('input[name="subscribe"]').is(':checked') ) {
-//     $('input[name="email"]').show();
-// }
-// else {
-//     $('input[name="email"]').hide();
-// }
-
 $('.onoffswitch-label').click(function() {
-  console.log("one " + thermostat.isPowerSave())
     if(thermostat.powerSaveStatus == false) {
-      console.log("on")
     thermostat.powerSaveOn();
   } else {
-    console.log("off")
       thermostat.powerSaveOff();
   }
-  console.log("two " + thermostat.isPowerSave())
   $('#powersaving').text(thermostat.isPowerSave());
-});
+})
 
   function updateTemperature() {
     $('#currenttemp').text(thermostat._temp);
+    $('#currenttemp').attr('class', thermostat.energyUsage());
   }
 });
-
-
-
